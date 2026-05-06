049dc6459c12b902482c67a055eb3bb396df21c7
detriste <detriste2018@gmail.com>
2026-05-06 14:12:44 -0300

    mudança de linguagem

---
+++ Saep/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Saep',
+  webDir: 'www'
+};
+
+export default config;
